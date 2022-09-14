18fca119918d65082aca9eb014ec485ad4586e49
unknown <threezinedine@gmail.com>
2022-09-14 18:55:51 +0700

    refactor the create query

--- src/main/cores/table.py
+++ src/main/cores/table.py
@@ -3,7 +3,8 @@ from .i_table import ITable
 
 class Table(ITable):
     def __init__(self, name="", columns=[]):
-        pass
+        self._name = name
+        self._columns = columns
 
     def get_create_query(self):
-        return "CREATE TABLE testing_table (id int)"
+        return f"CREATE TABLE {self._name} (id int)"
