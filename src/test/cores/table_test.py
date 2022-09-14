40623452f292219838195aa02dc3d757fbfae113
unknown <threezinedine@gmail.com>
2022-09-14 19:13:07 +0700

    add test for 2 columns table creation

--- src/test/cores/table_test.py
+++ src/test/cores/table_test.py
@@ -28,3 +28,18 @@ class TableTest(unittest.TestCase):
 
         assert query == f"CREATE TABLE testing_table (id int)"
 
+
+    def test_get_create_query_method_with_id_int_columns(self):
+        id_column = Mock(spec=IColumn)
+        id_column.get_definition.return_value = "id int"
+        name_column = Mock(spec=IColumn) 
+        name_column.get_definition.return_value = "name varchar(23)"
+
+        self.table = Table(name=self.table_name, columns=[
+                id_column, name_column
+            ])
+
+        query = self.table.get_create_query()
+
+        assert query == f"CREATE TABLE testing_table (id int, name varchar(23))"
+
